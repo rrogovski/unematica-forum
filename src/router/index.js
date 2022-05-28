62c2d6278592e97e25ce80eacd23ce38470ddc8b
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-05-28 15:59:21 -0400

    Navigation guards router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageNotFound from '@/components/PageNotFound'
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -13,7 +14,21 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+
+      if (threadExists) {
+        next()
+      } else {
+        next({
+          name: 'notFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
