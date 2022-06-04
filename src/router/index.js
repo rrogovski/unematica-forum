8c4a3b1d99ebc1be6a689c9333446e78029d2226
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-06-04 12:26:10 -0400

    Scroll to top after update profile

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,11 @@ const routes = [
   {
     path: '/me',
     name: 'Profile',
-    component: Profile
+    component: Profile,
+    meta: {
+      toTop: true,
+      smoothScroll: true
+    }
   },
   {
     path: '/me/edit',
@@ -66,7 +70,16 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior (to) {
+    const scroll = {}
+
+    if (to.meta.toTop) scroll.top = 0
+
+    if (to.meta.smoothScroll) scroll.behavior = 'smooth'
+
+    return scroll
+  }
 })
 
 export default router
