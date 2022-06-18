ef7cdfe16fdf49a501f6af124de688278761a2d5
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-06-18 16:51:40 -0400

    Showing a Progress Bar while Components Fetch Asynchronous Data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <the-navbar />
   <div class="container">
-    <router-view v-show="showPage" @ready="showPage = true" />
+    <router-view v-show="showPage" @ready="onPageReady" />
     <app-spinner-pacman v-show="!showPage" />
   </div>
 </template>
@@ -9,6 +9,8 @@
 <script>
 import { mapActions } from 'vuex'
 import TheNavbar from '@/components/TheNavbar.vue'
+import NProgress from 'nprogress'
+
 export default {
   components: { TheNavbar },
   name: 'App',
@@ -18,20 +20,33 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['fetchAuthUser'])
+    ...mapActions(['fetchAuthUser']),
+    onPageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
   },
   created () {
     this.fetchAuthUser()
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
     this.$router.beforeEach(() => {
       this.showPage = false
+      NProgress.start()
     })
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "assets/style.css";
+  @import "~nprogress/nprogress.css";
 
+  #nprogress .bar {
+    background: #4b81df !important;
+  }
   .container {
     padding-top: 80px;
   }
