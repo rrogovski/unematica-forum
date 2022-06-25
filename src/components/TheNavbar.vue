fc9f1b766504c6e0dce37e8ba7bb202e8c211391
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-06-25 12:35:28 -0400

    Using Class Bindings for User Drop Down Menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -17,26 +17,27 @@
         <ul>
 
             <li v-if="authUser" class="navbar-user">
-                <router-link :to="{ name: 'Profile'}">
+                <a @click.prevent="userDropdownOpen = !userDropdownOpen">
                     <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} imagem de perfil`">
                     <span>
                         {{ authUser.name }}
                         <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt="">
                     </span>
-                </router-link>
+                </a>
 
                 <!-- dropdown menu -->
                 <!-- add class "active-drop" to show the dropdown -->
-                <div id="user-dropdown">
+                <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
                     <div class="triangle-drop"></div>
                     <ul class="dropdown-menu">
-                        <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
-                        <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+                        <li class="dropdown-menu-item">
+                          <router-link :to="{ name: 'Profile'}">Ver perfil</router-link>
+                        </li>
+                        <li class="dropdown-menu-item">
+                          <a @click.prevent="$store.dispatch('signOut')">Sair</a>
+                        </li>
                     </ul>
                 </div>
-            </li>
-            <li v-if="authUser" class="navbar-item">
-              <a @click.prevent="$store.dispatch('signOut')">Sair</a>
             </li>
             <li v-if="!authUser" class="navbar-item">
               <router-link :to="{ name: 'SignIn' }">Entrar</router-link>
@@ -74,6 +75,11 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data () {
+    return {
+      userDropdownOpen: false
+    }
+  },
   computed: {
     ...mapGetters(['authUser'])
   }
